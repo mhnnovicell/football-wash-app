e3125d158b90324bd099cd53f9373ebdf7ff1555
mhn2012MikkelsPC\Mikkel Hornbech <mikkelhn1@hotmail.com>
2024-01-20 16:01:33 +0100

    Added some spacing

--- src/components/SelectPlayers.vue
+++ src/components/SelectPlayers.vue
@@ -89,7 +89,7 @@
         <div class="text-sm font-normal ms-3">Item moved successfully.</div>
         <button
           type="button"
-          class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+          class="mb-4 ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
           data-dismiss-target="#toast-success"
           aria-label="Close"
         >
